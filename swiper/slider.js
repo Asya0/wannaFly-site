f4ad10aee2310636db82029b80b0d17938340c72
Anuar <amendubaev@yandex.ru>
2024-07-25 00:01:54 +0600

    feat: added swiper js

---
+++ swiper/slider.js
@@ -0,0 +1,33 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+
+const swiper = new Swiper('.swiper', {
+    modules: [Navigation],
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+
+    // If we need pagination
+    // pagination: {
+    //     el: '.swiper-pagination',
+    // },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+    loop: false,
+    speed: 800,
+    grabCursor: true,
+
+});
